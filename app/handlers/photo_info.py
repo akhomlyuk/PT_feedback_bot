b051ac76772c0199825655a0d5bf20ac6ab75287
akhomlyuk <relicbs@gmail.com>
2024-03-14 12:47:07 +0300

    added photo support

--- app/handlers/photo_info.py
+++ app/handlers/photo_info.py
@@ -2,6 +2,8 @@ from aiogram import Router, F
 from aiogram.types import Message, FSInputFile
 import app.config.cfg as cfg
 import logging
+import json
+
 router: Router = Router()
 
 
@@ -10,8 +12,28 @@ async def bot_get_photo_info(message: Message):
     if message.from_user.id != 539491282:
         logging.info(message.from_user)
     try:
+        msg_obj = message.model_dump(mode='python')
+        msg_obj_ex = message.model_dump(mode='python', exclude={'from_user', 'chat', 'date'})
+        from_user = {}
+        chat = {}
+        data = {}
+        for key, value in msg_obj_ex.items():
+            if value is not None:
+                data[key] = value
+        for key, value in msg_obj['from_user'].items():
+            if value is not None:
+                from_user[key] = value
+        for key, value in msg_obj['chat'].items():
+            if value is not None:
+                chat[key] = value
+
         photo_id = message.photo[-1].file_id
+        photo_caption = message.caption
         await cfg.bot.send_photo(cfg.admins[0], photo_id)
+        await cfg.bot.send_message(cfg.admins[0], photo_caption)
+        await cfg.bot.send_message(cfg.admins[0], f'\n\n<b>ℹ️ От пользователя</b>: {from_user.get("first_name")} @{chat.get("username")}'
+                                                  f'\n<b>💬 Сообщение</b>: {data.get("text")}',
+                                   parse_mode='HTML')
     except Exception as e:
         logging.warning(e)
         await message.answer(f'{e}')
